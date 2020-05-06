f856200e9191216cdde191ab4fc4af509e974b8e
Mohammad Nouman <m.shafqat@wsu.edu>
2020-05-06 07:10:58 -0700

    removed commented code

--- PythonApplication.py
+++ PythonApplication.py
@@ -37,45 +37,45 @@ DG.add_weighted_edges_from(edges)
 print(f"Nodes: {DG.number_of_nodes()}")
 print(f"Edges: {DG.number_of_edges()}")
 
-# build a lookup of review counts and average trust rating
-#review_counts = df_.groupby('target').rating.count()
-#print(review_counts)
-#average_reviews = df_.groupby('target').rating.mean()
-#print(average_reviews)
+ build a lookup of review counts and average trust rating
+review_counts = df_.groupby('target').rating.count()
+print(review_counts)
+average_reviews = df_.groupby('target').rating.mean()
+print(average_reviews)
 
 
-#def get_size(user_id, m=200, min=300):
-#    s = review_counts.get(user_id)
-#    if s is not None:
-#        return min * s
-#    else:
-#        return min
+def get_size(user_id, m=200, min=300):
+    s = review_counts.get(user_id)
+    if s is not None:
+        return min * s
+    else:
+        return min
 
-#def get_color(user_id):
-#    s = average_reviews.get(user_id)
-#    if s is None:
-#        return 0.5
-#    # need a scaling function to translate -10-10 to 0.0-1.0 for colors maps
-#    return np.interp(s, (-10, 10), (0, 1))
+def get_color(user_id):
+    s = average_reviews.get(user_id)
+    if s is None:
+        return 0.5
+    # need a scaling function to translate -10-10 to 0.0-1.0 for colors maps
+    return np.interp(s, (-10, 10), (0, 1))
 
-#plt.figure(figsize=(14, 14)) 
-#plt.title("Bitcoin Trust Network", fontsize=18)
+plt.figure(figsize=(14, 14)) 
+plt.title("Bitcoin Trust Network", fontsize=18)
 
-#untrusted = mpatches.Patch(color='red', label='Negative Reputation')
-#trusted = mpatches.Patch(color='green', label='Positive Reputation')
-#size = mpatches.Patch(color='white', label='Node Size - Rating')
-#edges = mpatches.Patch(color='white', label='Edge Length - Indicator of Trust Inversely')
-#plt.legend(handles=[trusted, untrusted, size], loc='lower right')
+untrusted = mpatches.Patch(color='red', label='Negative Reputation')
+trusted = mpatches.Patch(color='green', label='Positive Reputation')
+size = mpatches.Patch(color='white', label='Node Size - Rating')
+edges = mpatches.Patch(color='white', label='Edge Length - Indicator of Trust Inversely')
+plt.legend(handles=[trusted, untrusted, size], loc='lower right')
 
-#pos = nx.spring_layout(DG, k=0.25)
-#sizes = [get_size(n) for n in DG]
-#colors = [get_color(n) for n in DG]
+pos = nx.spring_layout(DG, k=0.25)
+sizes = [get_size(n) for n in DG]
+colors = [get_color(n) for n in DG]
 
-#nc = nx.draw_networkx_nodes(DG, pos, nodelist=DG.nodes(), node_size=sizes, linewidths=2.0, node_color=colors, cmap=plt.cm.RdYlGn, alpha=0.8)
+nc = nx.draw_networkx_nodes(DG, pos, nodelist=DG.nodes(), node_size=sizes, linewidths=2.0, node_color=colors, cmap=plt.cm.RdYlGn, alpha=0.8)
 
-#ec = nx.draw_networkx_edges(DG, pos, arrows=True, alpha=0.08)
-#ax = plt.axis('off')
-#plt.show()
+ec = nx.draw_networkx_edges(DG, pos, arrows=True, alpha=0.08)
+ax = plt.axis('off')
+plt.show()
 
 import cpalgorithm as cp
 
@@ -88,7 +88,7 @@ x = km.get_coreness()
 #    print(k, v)
 
 print("END! END! END! END! ")
-sig_pair_id, sig_coreness, significance, p_values = cp.qstest(c, x, DG, km)
+#sig_pair_id, sig_coreness, significance, p_values = cp.qstest(c, x, DG, km)
 
 #x_sorted = {k: v for k, v in sorted(x.items(), key=lambda y: y[1])}
 
